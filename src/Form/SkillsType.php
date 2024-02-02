4be5cfe88e51d07b13f1bb181ee8bd07b543bc4b
stephm <stephanie2510@gmail.com>
2024-02-02 17:29:37 +0100

    form new skill

--- src/Form/SkillsType.php
+++ src/Form/SkillsType.php
@@ -2,25 +2,49 @@
 
 namespace App\Form;
 
-use App\Entity\Projects;
 use App\Entity\Skills;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Projects;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class SkillsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('cathegory')
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => 'name'
+                ],
+                'label' => 'Nom du skill',
+                'label_attr' => [
+                    'class' => 'name_label'
+                ],
+            ])
+            ->add('cathegory', TextType::class, [
+                'attr' => [
+                    'class' => 'category'
+                ],
+                'label' => 'Catégorie',
+                'label_attr' => [
+                    'class' => 'cat_label'
+                ],
+            ])
             //->add('projects', EntityType::class, [
                 //'class' => Projects::class,
                 //'choice_label' => 'id',
                 //'multiple' => true,
             //])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary'
+                ],
+                'label' => 'Valider'
+            ])
         ;
     }
 
